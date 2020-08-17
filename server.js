3d0a65620b167ad95210cd8d60d8861a2da5d4e7
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-17 11:12:22 -0400

    added untracked files

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client/build"));
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/react-portfolio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
