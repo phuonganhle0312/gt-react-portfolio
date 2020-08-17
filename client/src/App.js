5252743f8e9fbd570f919a22220c4c06693ca721
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-17 11:42:28 -0400

    added contact.jsx and app.js

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NoMatch from "./components/NoMatch/NoMatch";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route exact path = "/" component= {Home} />
+        <Route exact path = "/contact" component= {Contact} />
+        <Route exact path = "/portfolio" component= {Portfolio} />
+        <Route component= {NoMatch}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/containers/Contact/contact.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Contact.css"
+
+class Contact extends Component {
+  render() {
+    return (
+        <form action="mailto:phuonganh.le0312@gmail.com" method="get" encType="text/plain">
+        <section className="container" id="contactform">
+          <section className="row justify-content-center">
+            <section className="col-md-8">
+              <section className="form-group">
+                <label htmlFor="Name">Name</label>
+                <input type="name" className="form-control" id="nameFormControl" placeholder="John Smith" />
+              </section>
+              <section className="form-group">
+                <label htmlFor="Email">Email</label>
+                <input type="email" className="form-control" id="emailFormControl" placeholder="example@gmail.com" />
+              </section>
+              <section className="form-group">
+                <label htmlFor="Message">Message</label>
+                <textarea className="form-control" id="messageFormControl" rows={6} defaultValue={""} />
+              </section>
+              <section className="col-auto my-1">
+                <button type="submit" className="btn btn-dark" value="Send">Submit</button>
+              </section>
+            </section></section></section></form>
+    );
+  }
+};
+
+export default Contact;
